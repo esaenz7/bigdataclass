e662a46b0000d29b445dffad6ae1fc15a6de22a9
esaenz7 <esaenz7@gmail.com>
2021-08-04 16:54:46 -0600

    minver misc

--- tarea1/programaestudiante.py
+++ tarea1/programaestudiante.py
@@ -29,7 +29,7 @@ spark = SparkSession.builder\
         .config('spark.ui.port', '4050')\
         .getOrCreate()
 
-spark.sparkContext.setLogLevel("OFF")
+spark.sparkContext.setLogLevel("ERROR")
 files = sys.argv
 
 dfCiclista = spark.read.csv(files[1], schema=StructType(\
