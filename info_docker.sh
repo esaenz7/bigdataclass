77ddcb408943ddee04bf911121461b12c47be5d9
esaenz7 <esaenz7@gmail.com>
2021-08-28 15:30:50 -0600

    minchg

--- info_docker.sh
+++ info_docker.sh
@@ -2,7 +2,8 @@
 #
 docker info
 docker network ls
-docker container ls
+docker network inspect bigdatanet |grep -e IP -e Name
 docker image ls
+docker container ls
 docker ps -a
 docker stats --format "table {{.Name}}\t{{.CPUPerc}} {{.MemUsage}}\t{{.NetIO}}"
