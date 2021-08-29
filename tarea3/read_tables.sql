42046df5801e723aa9237c89241af69b42c47b1c
esaenz7 <esaenz7@gmail.com>
2021-08-28 18:32:53 -0600

    majver chg tarea3

--- tarea3/read_tables.sql
+++ tarea3/read_tables.sql
@@ -1,8 +1,16 @@
 BEGIN;
 
+\dt[+] tarea3;
 SELECT * FROM tarea3 LIMIT (5);
+SELECT count(*) FROM tarea3;
+\dt[+] modelo1;
 SELECT * FROM modelo1 LIMIT (5);
+SELECT count(*) FROM modelo1;
+\dt[+] modelo2;
 SELECT * FROM modelo2 LIMIT (5);
+SELECT count(*) FROM modelo2;
+\dt[+] modelo3;
 SELECT * FROM modelo3 LIMIT (5);
+SELECT count(*) FROM modelo3;
 
 COMMIT;
--- tarea3/run_main.sh
+++ tarea3/run_main.sh
@@ -3,7 +3,6 @@
 PGPASSWORD=testPassword psql -h 10.7.84.102 -U postgres -p 5432 < create_tables.sql
 #
 jupyter notebook \
-    # &
-    # --notebook-dir /host_data/  \
+    # & --notebook-dir /host_data/  \
     --ip=0.0.0.0 --port=8888 --allow-root
 #
