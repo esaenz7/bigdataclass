5c3a70de7c766c13321cef78c5426140e63cf0a5
esaenz7 <esaenz7@gmail.com>
2021-09-01 20:46:20 -0600

    majver chgs sql queries

--- proyecto/create_tables.sql
+++ proyecto/create_tables.sql
@@ -1,60 +1,63 @@
 BEGIN;
 
-DROP TABLE IF EXISTS tarea3;
-CREATE TABLE tarea3 (
-  id SERIAL PRIMARY KEY,
-  vmag NUMERIC (32,16) NULL,
-  plx NUMERIC (32,16) NULL,
-  bv NUMERIC (32,16) NULL,
-  amag NUMERIC (32,16) NULL,
-  class INTEGER NULL
+DROP TABLE IF EXISTS tb_proyecto;
+CREATE TABLE tb_proyecto (
+id SERIAL PRIMARY KEY,
+carrier VARCHAR(50) NULL,
+sdephr NUMERIC (32,16) NULL,
+sarrhr NUMERIC (32,16) NULL,
+dyofwk NUMERIC (32,16) NULL,
+wkofyr NUMERIC (32,16) NULL,
+wtyp VARCHAR(50) NULL,
+wsev VARCHAR(50) NULL,
+depdel NUMERIC (32,16) NULL,
+txout NUMERIC (32,16) NULL,
+selap NUMERIC (32,16) NULL,
+dist NUMERIC (32,16) NULL,
+evtim NUMERIC (32,16) NULL,
+label INTEGER NULL
 );
 
-DROP TABLE IF EXISTS modelo1;
-CREATE TABLE modelo1 (
-  id SERIAL PRIMARY KEY,
-  vmag NUMERIC (32,16) NULL,
-  plx NUMERIC (32,16) NULL,
-  bv NUMERIC (32,16) NULL,
-  amag NUMERIC (32,16) NULL,
-  class INTEGER NULL,
-  prediccion INTEGER NULL
+DROP TABLE IF EXISTS tb_flights;
+CREATE TABLE tb_flights (
+id SERIAL PRIMARY KEY,
+date DATE NULL,
+orig VARCHAR(50) NULL,
+dest VARCHAR(50) NULL,
+carrier VARCHAR(50) NULL,
+sdeptim NUMERIC (32,16) NULL,
+depdel NUMERIC (32,16) NULL,
+txout NUMERIC (32,16) NULL,
+sarrtim NUMERIC (32,16) NULL,
+selap NUMERIC (32,16) NULL,
+dist NUMERIC (32,16) NULL,
+dyofwk NUMERIC (32,16) NULL,
+wkofyr NUMERIC (32,16) NULL,
+sdephr NUMERIC (32,16) NULL,
+sarrhr NUMERIC (32,16) NULL,
+label INTEGER NULL
 );
 
-DROP TABLE IF EXISTS modelo2;
-CREATE TABLE modelo2 (
-  id SERIAL PRIMARY KEY,
-  vmag NUMERIC (32,16) NULL,
-  plx NUMERIC (32,16) NULL,
-  bv NUMERIC (32,16) NULL,
-  amag NUMERIC (32,16) NULL,
-  class INTEGER NULL,
-  prediccion INTEGER NULL
+DROP TABLE IF EXISTS tb_airports;
+CREATE TABLE tb_airports (
+id SERIAL PRIMARY KEY,
+iata VARCHAR(50) NULL,
+icao VARCHAR(50) NULL
 );
 
-DROP TABLE IF EXISTS modelo3;
-CREATE TABLE modelo3 (
-  id SERIAL PRIMARY KEY,
-  vmag NUMERIC (32,16) NULL,
-  plx NUMERIC (32,16) NULL,
-  bv NUMERIC (32,16) NULL,
-  amag NUMERIC (32,16) NULL,
-  class INTEGER NULL,
-  prediccion INTEGER NULL
-);
-
-DROP TABLE IF EXISTS temp;
-CREATE TABLE temp (
-  id SERIAL PRIMARY KEY,
-  vmag NUMERIC (32,16) NULL,
-  plx NUMERIC (32,16) NULL,
-  bv NUMERIC (32,16) NULL,
-  amag NUMERIC (32,16) NULL,
-  class INTEGER NULL
+DROP TABLE IF EXISTS tb_weather;
+CREATE TABLE tb_weather (
+id SERIAL PRIMARY KEY,
+date DATE NULL,
+wtyp VARCHAR(50) NULL,
+wsev VARCHAR(50) NULL,
+icao VARCHAR(50) NULL,
+evhr NUMERIC (32,16) NULL,
+evtim NUMERIC (32,16) NULL
 );
 
 SELECT table_name, column_name, data_type, numeric_precision, numeric_scale
-FROM information_schema.columns WHERE TABLE_NAME SIMILAR TO ('tarea%|modelo%|temp%')
+FROM information_schema.columns WHERE TABLE_NAME SIMILAR TO ('tb%|')
 ORDER BY 1;
 
 \dt[+]
--- proyecto/read_tables.sql
+++ proyecto/read_tables.sql
@@ -1,16 +1,16 @@
 BEGIN;
 
-\dt[+] tarea3;
-SELECT * FROM tarea3 LIMIT (5);
-SELECT count(*) FROM tarea3;
-\dt[+] modelo1;
-SELECT * FROM modelo1 LIMIT (5);
-SELECT count(*) FROM modelo1;
-\dt[+] modelo2;
-SELECT * FROM modelo2 LIMIT (5);
-SELECT count(*) FROM modelo2;
-\dt[+] modelo3;
-SELECT * FROM modelo3 LIMIT (5);
-SELECT count(*) FROM modelo3;
+\dt[+] tb_proyecto;
+SELECT * FROM tb_proyecto LIMIT (5);
+SELECT count(*) FROM tb_proyecto;
+\dt[+] tb_flights;
+SELECT * FROM tb_flights LIMIT (5);
+SELECT count(*) FROM tb_flights;
+\dt[+] tb_airports;
+SELECT * FROM tb_airports LIMIT (5);
+SELECT count(*) FROM tb_airports;
+\dt[+] tb_weather;
+SELECT * FROM tb_weather LIMIT (5);
+SELECT count(*) FROM tb_weather;
 
 COMMIT;
