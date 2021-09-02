4ad174653c3176b461b7d79ba89f94ebaaff7b24
esaenz7 <esaenz7@gmail.com>
2021-09-02 16:13:18 -0600

    chgs to better read tables

--- proyecto/read_tables.sql
+++ proyecto/read_tables.sql
@@ -1,19 +1,30 @@
 BEGIN;
-
-\dt[+] tb_proyecto;
-SELECT * FROM tb_proyecto LIMIT (5);
+\echo
+\echo 'tb_proyecto'
 SELECT count(*) FROM tb_proyecto;
-\dt[+] tb_proyecto_v2;
-SELECT * FROM tb_proyecto_v2 LIMIT (5);
+SELECT * FROM tb_proyecto LIMIT (5);
+\echo
+\echo 'tb_proyecto_v2'
 SELECT count(*) FROM tb_proyecto_v2;
-\dt[+] tb_flights;
-SELECT * FROM tb_flights LIMIT (5);
+SELECT * FROM tb_proyecto_v2 LIMIT (5);
+\echo
+\echo 'tb_flights'
 SELECT count(*) FROM tb_flights;
-\dt[+] tb_airports;
-SELECT * FROM tb_airports LIMIT (5);
+SELECT * FROM tb_flights LIMIT (5);
+\echo
+\echo 'tb_airports'
 SELECT count(*) FROM tb_airports;
-\dt[+] tb_weather;
-SELECT * FROM tb_weather LIMIT (5);
+SELECT * FROM tb_airports LIMIT (5);
+\echo
+\echo 'tb_weather'
 SELECT count(*) FROM tb_weather;
-
+SELECT * FROM tb_weather LIMIT (5);
+\echo
+\echo
+\dt[+] tb_proyecto;
+\dt[+] tb_proyecto_v2;
+\dt[+] tb_flights;
+\dt[+] tb_airports;
+\dt[+] tb_weather;
+\echo
 COMMIT;
