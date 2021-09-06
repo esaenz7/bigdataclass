f989084da432adff3f11856964458440efbda88e
esaenz7 <esaenz7@gmail.com>
2021-09-06 16:37:17 -0600

    run jt and stop server 8888 before start

--- proyecto/run_jupyter.sh
+++ proyecto/run_jupyter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+jt -r
+jt -t chesterish -T -N -cellw 100%
+#
+jupyter notebook stop 8888
 jupyter notebook \
     --ip=0.0.0.0 --port=8888 --allow-root &
     # --notebook-dir /host_data/ \
